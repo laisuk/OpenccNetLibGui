17c696cdaef8e650e198953f31e918cae26f5424
laisuk <laisuk@yahoo.com>
2026-01-01 00:15:10 +0800

    Added About Dialog Box

--- ViewModels/AboutViewModel.cs
+++ ViewModels/AboutViewModel.cs
@@ -1,35 +1,34 @@
-﻿using System.Diagnostics;
-using System.Reactive;
+﻿using System.Reactive;
 using ReactiveUI;
 
 namespace OpenccNetLibGui.ViewModels;
 
 public sealed class AboutViewModel : ReactiveObject
 {
-    public string AppName => "OpenccNetLibGui";
+    public static string AppName => "OpenccNetLibGui";
 
-    public string Version =>
+    public static string Version =>
         typeof(AboutViewModel).Assembly
             .GetName().Version?.ToString() ?? "Unknown";
 
-    public string Description =>
+    public static string Description =>
         "Chinese Simplified / Traditional Converter\nPowered by OpenccNetLib + Pdfium";
 
-    public string PdfEngine => "Pdfium (native)";
+    public static string PdfEngine => "Pdfium (native)";
 
-    public ReactiveCommand<Unit, Unit> OpenGitHub { get; }
-        = ReactiveCommand.Create(() =>
-        {
-            Process.Start(new ProcessStartInfo
-            {
-                FileName = "https://github.com/laisuk/OpenccNetLibGui",
-                UseShellExecute = true
-            });
-        });
+    // public ReactiveCommand<Unit, Unit> OpenGitHub { get; }
+    //     = ReactiveCommand.Create(() =>
+    //     {
+    //         Process.Start(new ProcessStartInfo
+    //         {
+    //             FileName = "https://github.com/laisuk/OpenccNetLibGui",
+    //             UseShellExecute = true
+    //         });
+    //     });
 
     public ReactiveCommand<Unit, Unit> Close { get; }
         = ReactiveCommand.Create(() => { });
-    
+
     // ✅ 保留 constructor，但唔做初始化
     public AboutViewModel()
     {
