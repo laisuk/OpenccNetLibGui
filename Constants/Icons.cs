3b0f9ab313ff8a87f6c4610569dc29cf8f1e7462
laisuk <laisuk@yahoo.com>
2026-04-18 04:27:59 +0800

    Added Fluent Icon Fonts

---
+++ Constants/Icons.cs
@@ -0,0 +1,22 @@
+﻿namespace OpenccNetLibGui.Constants;
+
+public static class Icons
+{
+    public const string Refresh = "\uE149";
+    public const string Play = "\uE102";
+    public const string Home = "\uE10F";
+    
+    public const string Copy = "\uE16F";
+    public const string Paste   = "\uE16D";
+    public const string Delete = "\uE107";
+
+    // Folder / Open / Save
+    public const string OpenFile   = "\uE1A5";
+    public const string OpenFolderHorizontal   = "\ued25";
+    public const string FolderOpen = "\uE838";
+    public const string Save = "\uE105";
+    public const string SaveAs = "\uE792";
+    
+    public const string AddTo = "\uecc8";
+    public const string RemoveFrom = "\uecc9";
+}
