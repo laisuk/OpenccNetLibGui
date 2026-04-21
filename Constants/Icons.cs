6865c626e1edd770c99366de35683f4455403aad
laisuk <laisuk@yahoo.com>
2026-04-21 13:43:09 +0800

    Update Exit button to use Power Button icon

--- Constants/Icons.cs
+++ Constants/Icons.cs
@@ -5,18 +5,23 @@ public static class Icons
     public const string Refresh = "\uE149";
     public const string Play = "\uE102";
     public const string Home = "\uE10F";
-    
+    public const string Info = "\uE946";
+    public const string PowerButton = "\uE7E8";
+
     public const string Copy = "\uE16F";
-    public const string Paste   = "\uE16D";
+    public const string Paste = "\uE16D";
     public const string Delete = "\uE107";
 
     // Folder / Open / Save
-    public const string OpenFile   = "\uE1A5";
-    public const string OpenFolderHorizontal   = "\ued25";
+    public const string OpenFile = "\uE1A5";
+    public const string OpenFolderHorizontal = "\uED25";
     public const string FolderOpen = "\uE838";
     public const string Save = "\uE105";
     public const string SaveAs = "\uE792";
-    
-    public const string AddTo = "\uecc8";
-    public const string RemoveFrom = "\uecc9";
+    public const string Edit = "\uE70F";
+    public const string CheckboxComposite = "\uE73A";
+    public const string Completed = "\uE930";
+
+    public const string AddTo = "\uECC8";
+    public const string RemoveFrom = "\uECC9";
 }
