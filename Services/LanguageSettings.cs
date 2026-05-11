3eaed6cafed3a755f9c33e277f4259fcd80259a6
laisuk <laisuk@yahoo.com>
2026-05-12 04:21:15 +0800

    Serde PunctuationChars as string instead of char to match System.Text.Json

--- Services/LanguageSettings.cs
+++ Services/LanguageSettings.cs
@@ -10,7 +10,7 @@ public class LanguageSettings
 {
     public List<Language>? Languages { get; set; }
     public int CharCheck { get; set; }
-    public Dictionary<char, char>? PunctuationChars { get; set; }
+    public Dictionary<string, string>? PunctuationChars { get; set; }
     public List<string>? TextFileTypes { get; set; }
     public List<string>? OfficeFileTypes { get; set; }
     public string? Dictionary { get; set; }
